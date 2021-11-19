3b165e6b4fa58a5fe498be54eaf8902833f72f3e
hodasalah <hodasalah35@gmail.com>
2021-11-19 19:29:50 +0200

    add average property

--- js/ratings.js
+++ js/ratings.js
@@ -14,7 +14,7 @@ function collect_ratings() {
 		ratings.sum += parseInt(element.value) * rating;
 	});
 	if (ratings.count !== 0) {
-		ratings.average = sum / count;
+		ratings.average = ratings.sum / ratings.count;
 	}
 	return ratings;
 }
